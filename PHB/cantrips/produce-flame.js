24ac0d22e3036fcbe47f4c709fa8bc20cf1188c6
Rupert Walker <RupertCW@gmail.com>
2021-10-19 17:39:32 +0300

    Update produce-flame.js

---
+++ PHB/cantrips/produce-flame.js
@@ -0,0 +1,10 @@
+let tokenD = canvas.tokens.get(args[0].tokenId);
+new Sequence()
+    .effect()
+        .file("jb2a.fire_bolt.orange")
+        .atLocation(tokenD)
+        .reachTowards(args[0].targets[0])
+        .missed(args[0].hitTargets.length === 0)
+        .filter("Glow", { color: 0xffffff })
+        .waitUntilFinished()
+    .play()
