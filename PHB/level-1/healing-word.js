735d14b8ba03524c022a0d2615316f29dc6d23dd
Rupert Walker <RupertCW@gmail.com>
2021-10-22 14:39:04 +0300

    Update healing-word.js

---
+++ PHB/level-1/healing-word.js
@@ -0,0 +1,39 @@
+const casterToken = canvas.tokens.controlled[0];
+if (!casterToken) {
+	ui.notifications.warn("Please select a valid token to use this ability.");
+	return;
+}
+const targetId = Array.from(game.user.targets)[0];
+if (!targetId) {
+	ui.notification.warn("This spell requires at least one valid target.");
+	return;
+}
+
+let myStringArray = Array.from(game.user.targets)[0];
+let arrayLength = game.user.targets.size;
+
+    for (let i = 0; i < arrayLength; i++) {
+    let targetId = Array.from(game.user.targets)[i];
+    new Sequence()
+        .effect()
+            .file("jb2a.extras.tmfx.runes.circle.outpulse.evocation")
+            .atLocation(casterToken)
+            .duration(1000)
+            .fadeIn(500)
+            .fadeOut(500)
+            .scale(0.5)
+            .waitUntilFinished(-500)
+            .filter("Glow", { color: 0xabcdef })
+        .effect()
+            .file("jb2a.impact.004.blue")
+            .atLocation(targetId)
+            .fadeIn(500)
+        .effect()
+            .file("jb2a.healing_generic.400px.blue")
+            .fadeIn(100)
+            .fadeOut(100)
+            .scale(0.4, 0.45)
+            .atLocation(targetId)
+            .randomRotation()
+    .play();
+}
