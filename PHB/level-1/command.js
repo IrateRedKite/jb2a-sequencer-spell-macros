536ac36f718266402a74a5b9e3c32d4edf49809e
Rupert Walker <RupertCW@gmail.com>
2021-10-22 11:23:30 +0300

    Update command.js

---
+++ PHB/level-1/command.js
@@ -0,0 +1,37 @@
+const casterToken = canvas.tokens.controlled[0];
+if (!casterToken) {
+	ui.notifications.warn("Please select a valid token to use this ability.");
+	return;
+}
+const targetId = Array.from(game.user.targets)[0];
+if (!targetId) {
+	ui.notification.warn("This spell requires at least one valid target.");
+	return;
+}
+
+    new Sequence()
+        .effect()
+            .file("jb2a.extras.tmfx.runes.circle.outpulse.enchantment")
+            .atLocation(casterToken)
+            .duration(4000)
+            .fadeIn(500)
+            .fadeOut(500)
+            .scale(0.5)
+            .waitUntilFinished(-2000)
+            .filter("Glow", { color: 0xc00000 })
+        .effect()
+            .file("jb2a.impact.004.dark_red")
+            .atLocation(targetId)
+            .fadeIn(500)
+        .effect()
+            .file("jb2a.extras.tmfx.runes.circle.simple.enchantment")
+            .atLocation(targetId)
+            .fadeIn(500)
+            .scaleIn(0, 300, {ease: "easeOutCubic"})
+            .scaleToObject(2)
+            .duration(2000)
+            .fadeOut(500)
+            .filter("Glow", { color: 0xc00000 })
+    .play();
+
+
