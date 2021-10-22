059c33fa738569ed543408dc0820f8a48f3b5e21
Rupert Walker <RupertCW@gmail.com>
2021-10-22 13:51:36 +0300

    Update comprehend-languages.js

---
+++ PHB/level-1/comprehend-languages.js
@@ -0,0 +1,32 @@
+const casterToken = canvas.tokens.controlled[0];
+if (!casterToken) {
+	ui.notifications.warn("Please select a valid token to use this ability.");
+	return;
+}
+
+new Sequence()
+    .effect()
+        .file("jb2a.extras.tmfx.runes.circle.inpulse.divination")
+        .atLocation(casterToken)
+        .duration(1700)
+        .fadeIn(500)
+        .fadeOut(500)
+        .scale(0.5)
+        .filter("Glow", { color: 0xabcdef })
+        .scaleIn(0, 500, {ease: "easeOutCubic", delay: 100})
+    .effect()
+        .file("jb2a.moonbeam.01.intro.blue")
+        .atLocation(casterToken)
+        .fadeIn(100)
+        .fadeOut(200)
+        .duration(1200)
+        .waitUntilFinished(-500)
+    .effect()
+        .file("jb2a.detect_magic.circle.grey")
+        .scale(0.45)
+        .atLocation(casterToken)
+        .fadeIn(500)
+        .fadeOut(500)
+        .duration(1500)
+        .randomRotation() 
+    .play();
