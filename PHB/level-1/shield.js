ff2b23e5b268bb42997db9dcfef90d3fba3dcddb
Rupert Walker <RupertCW@gmail.com>
2021-10-23 09:26:16 +0300

    Update shield.js

---
+++ PHB/level-1/shield.js
@@ -0,0 +1,33 @@
+let tokenD = canvas.tokens.get(args[1].tokenId);
+
+if(args[0] === "on"){
+     // If the dynamic active effect started
+    new Sequence()
+        .effect()
+            .file("jb2a.shield.01.intro.blue")
+            .attachTo(tokenD)
+            .scale(0.5)
+            .waitUntilFinished(-500)
+        .effect()
+            .file("jb2a.shield.01.loop.blue")
+            .attachTo(tokenD)
+            .scale(0.5)
+            .persist()
+            .name(`Shield-${tokenD.id}`)
+            .fadeIn(300)
+            .fadeOut(300)
+            .extraEndDuration(800)
+        .play()
+}
+
+if(args[0] === "off"){
+    // If the dynamic active effect ended
+    Sequencer.EffectManager.endEffects({ name: `Shield-${tokenD.id}`, object: tokenD });
+
+    new Sequence()
+        .effect()
+            .file("jb2a.shield.01.outro_explode.blue")
+            .scale(0.5)
+            .attachTo(tokenD)
+        .play()
+}
