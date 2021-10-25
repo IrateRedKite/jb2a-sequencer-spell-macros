c640982e023c7972f15312898f7d13f010247871
Rupert Walker <RupertCW@gmail.com>
2021-10-25 14:19:50 +0300

    Update knock.js

---
+++ PHB/level-2/knock.js
@@ -0,0 +1,45 @@
+const casterToken = canvas.tokens.controlled[0];
+if (!casterToken) {
+	ui.notifications.warn("Please select a valid token to use this ability.");
+	return;
+}
+const targetId = Array.from(game.user.targets)[0];
+if (!targetId) {
+	ui.notification.warn("This spell requires at least one valid target.");
+	return;
+}
+
+    new Sequence()
+        .effect()
+            .file("jb2a.energy_strands.overlay.purple.01")
+            .delay(200)
+            .fadeIn(300)
+            .fadeOut(500)
+            .duration(3000)
+            .scale(0.4)
+            .atLocation(casterToken)
+            .scaleIn(0, 500, {ease: "easeOutCubic"})    
+        .effect()
+            .file("jb2a.extras.tmfx.runes.circle.outpulse.tramnsmutation")
+            .atLocation(casterToken)
+            .duration(4000)
+            .fadeIn(500)
+            .fadeOut(500)
+            .scale(0.5)
+            .waitUntilFinished(-2000)
+            .filter("Glow", { color: 0x800080 })    
+        .effect()
+            .file("jb2a.moonbeam.01.intro.rainbow")
+            .atLocation(targetId)
+            .fadeIn(100)
+            .fadeOut(200)
+            .duration(1200)
+            .belowTokens()
+            .waitUntilFinished(-500)        
+        .effect()
+            .file("jb2a.detect_magic.circle.purple")
+            .atLocation(casterToken)
+            .fadeIn(500)
+            .fadeOut(500)
+            .randomRotation() 
+    .play();
