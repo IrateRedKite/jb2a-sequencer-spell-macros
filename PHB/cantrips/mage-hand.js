351462aba73ee904dd5a172c4758a4eb06f60326
Rupert Walker <RupertCW@gmail.com>
2021-10-25 13:52:32 +0300

    Create mage-hand.js

---
+++ PHB/cantrips/mage-hand.js
@@ -0,0 +1,43 @@
+const casterToken = canvas.tokens.controlled[0];
+if (!casterToken) {
+	ui.notifications.warn("Please select a valid token to use this ability.");
+	return;
+}
+
+await warpgate.spawn("Mage Hand", {}, {
+    pre: async (location) => {
+        const seq = new Sequence()
+        .effect()
+        .file("jb2a.extras.tmfx.runes.circle.outpulse.conjuration")
+        .atLocation(casterToken)
+        .duration(2000)
+        .fadeIn(500)
+        .fadeOut(500)
+        .scale(0.5)
+        .opacity(0.3)
+        .filter("Glow", { color: 0xa1c4fd })
+        .scaleIn(0, 500, {ease: "easeOutCubic", delay: 100})
+    .effect()
+        .file("jb2a.moonbeam.01.intro.blue")
+        .atLocation(casterToken)
+        .fadeIn(100)
+        .fadeOut(200)
+        .duration(1200)
+        .waitUntilFinished(-500)
+    .effect()
+        .file("jb2a.toll_the_dead.blue.shockwave")
+        .atLocation(location)
+        .fadeIn(500)
+        .fadeOut(500)
+        .scale(0.5)
+        .scaleIn(0, 500, {ease: "easeOutCubic", delay: 100})
+        .filter("Glow", { color: 0xa1c4fd })
+
+    seq.play();
+
+        // Sleep for 500ms
+        await (new Promise(resolve => setTimeout(resolve, 1200)));
+    }
+}, {
+    collision: false
+});
