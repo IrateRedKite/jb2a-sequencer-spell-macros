9bb82b27276c59552ad2f335c3724caf2d392bf2
Rupert Walker <RupertCW@gmail.com>
2021-10-25 14:14:04 +0300

    Update feather-fall.js

---
+++ PHB/level-1/feather-fall.js
@@ -0,0 +1,41 @@
+const casterToken = canvas.tokens.controlled[0];
+if (!casterToken) {
+	ui.notifications.warn("Please select a valid token to use this ability.");
+	return;
+}
+const targetId = Array.from(game.user.targets)[0];
+if (!targetId) {
+	ui.notification.warn("This spell requires at least one valid target.");
+	return;
+}
+
+let myStringArray = Array.from(game.user.targets)[0];
+let arrayLength = game.user.targets.size;
+
+    for (let i = 0; i < arrayLength; i++) {
+    let targetId = Array.from(game.user.targets)[i];
+    new Sequence()
+        .effect()
+            .file("jb2a.moonbeam.01.intro.rainbow")
+            .atLocation(casterToken)
+            .fadeIn(100)
+            .fadeOut(500)
+            .duration(1200) 
+        .effect()
+            .file("jb2a.extras.tmfx.runes.circle.outpulse.tramnsmutation")
+            .atLocation(casterToken)
+            .duration(2000)
+            .fadeIn(500)
+            .fadeOut(500)
+            .scale(0.5)
+            .waitUntilFinished(-500)
+            .filter("Glow", { color: 0xffffbf })
+        .effect()
+            .file("jb2a.impact.007.yellow")
+            .fadeIn(100)
+            .fadeOut(100)
+            .scale(0.4, 0.45)
+            .atLocation(targetId)
+            .randomRotation()
+    .play();
+}
