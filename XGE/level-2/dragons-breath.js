0bcd439f3d3c3d5943c5b59c396b26b657ff6fce
Rupert Walker <RupertCW@gmail.com>
2021-11-03 20:08:38 +0300

    Update dragons-breath.js

--- XGE/level-2/dragons-breath.js
+++ XGE/level-2/dragons-breath.js
@@ -16,7 +16,7 @@ if(args[0] === "on"){
     await targetId.actor.createOwnedItem({
 		name: spellName,
 		type: "spell",
-        img: "icons/magic/fire/beam-jet-stream-blue.webp",
+        img: "icons/creatures/abilities/dragon-fire-breath-orange.webp",
 		data: {
             description: {
                 value: "<div class=\"rd__b  rd__b--3\"><p>You touch one willing creature and imbue it with the power to spew magical energy from its mouth, provided it has one. Choose acid, cold, fire, lightning, or poison. Until the spell ends, the creature can use an action to exhale energy of the chosen type in a 15-foot cone. Each creature in that area must make a Dexterity saving throw, taking [[/r 3d6]] damage of the chosen type on a failed save, or half as much damage on a successful one.</p><div class=\"rd__spc-inline-post\"></div></div><div class=\"rd__b  rd__b--3\"><div class=\"rd__b  rd__b--3\"><span class=\"rd__h rd__h--3\" data-title-index=\"5947\"> <span class=\"entry-title-inner help-subtle\">At Higher Levels.</span></span> <p>When you cast this spell using a spell slot of 3rd level or higher, the damage increases by [[/r 1d6]] for each slot level above 2nd.</p><div class=\"rd__spc-inline-post\"></div></div><div class=\"rd__spc-inline-post\"></div></div>",
@@ -43,6 +43,7 @@ if(args[0] === "on"){
 			level: 0,
             school: "trs",
             actionType: "save",
+            source:"XGE",
 		},
     })
 
