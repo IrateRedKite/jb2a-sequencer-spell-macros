83f3878de514c67ce1f02c9be76946c4ed18cc67
Rupert Walker <RupertCW@gmail.com>
2021-11-05 14:10:35 +0300

    Create dragons-breath-active-item.js

---
+++ XGE/level-2/dragons-breath-active-item.js
@@ -0,0 +1,33 @@
+const template = canvas.templates.placeables[canvas.templates.placeables.length-1];
+let damageType = args[0].item.data.damage.parts[0][1]
+let damageVisualEffect;
+
+switch (damageType) {
+    case "acid":
+        damageVisualEffect = "jb2a.breath_weapons.poison.cone.green";
+        break;
+    case "cold":
+        damageVisualEffect = "jb2a.breath_weapons.cold.cone.blue";
+        break;
+    case "fire":
+        damageVisualEffect = "jb2a.breath_weapons.fire.cone.orange.01";
+        break;
+    case "lightning":
+        damageVisualEffect = "jb2a.breath_weapons.lightning.line.blue";
+        break;
+    case "poison":
+        damageVisualEffect = "jb2a.breath_weapons.poison.cone.purple";
+        break;
+    default:
+        damageVisualEffect = "jb2a.breath_weapons.fire.cone.orange.01";
+        break;
+}
+
+new Sequence()
+    .effect()
+        .file(damageVisualEffect)
+        .atLocation(template, { cacheLocation: true })
+        .reachTowards(template, { cacheLocation: true })      
+.play();
+
+canvas.scene.deleteEmbeddedDocuments("MeasuredTemplate", [template.data._id]);
