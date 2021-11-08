5bc43a15125ee7ded4dca0b7cea982b445c8c412
Paul Bierton <paul.bierton@vulcan-eng.com>
2021-11-08 21:57:44 +0000

    Shadow of Moil Attack Update

--- XGE/level-4/shadow-of-moil-attack.js
+++ XGE/level-4/shadow-of-moil-attack.js
@@ -1,3 +1,5 @@
+// remove the damage from the Shadow of Moil Chat Card, Create a new item and add the damage to it then apply the attack macro to that
+
 const casterToken = canvas.tokens.get(args[0].tokenId);
 
 if (!casterToken) {
@@ -9,14 +11,14 @@ let damageTotal = game.modules.get("midi-qol").damageRoll?.total ?? 16;
 
 new Sequence()
 
-.effect()
+    .effect()
     .file("jb2a.energy_strands.complete.dark_red.01")
     .atLocation(casterToken)
     .fadeIn(500)
     .fadeOut(500)
     .scaleToObject()
 
-.effect()
+    .effect()
     .file("jb2a.energy_strands.range.standard.dark_red.01")
     .repeats(Math.max(1, Math.floor(damageTotal / 2)), 100, 200)
     .randomizeMirrorY()
