5a66359bade063801d0c4204db3ebb7a58953fe0
Rupert Walker <RupertCW@gmail.com>
2021-11-12 16:48:18 +0300

    Create jump.js

---
+++ PHB/level-1/jump.js
@@ -0,0 +1,35 @@
+const casterToken = canvas.tokens.get(args[0].tokenId);
+if (!casterToken) {
+	ui.notifications.warn("Please select a valid token to use this ability.");
+	return;
+}
+const targetId = Array.from(game.user.targets)[0];
+if (!targetId) {
+	ui.notification.warn("This spell requires at least one valid target.");
+	return;
+}
+
+    new Sequence()
+        .effect()
+            .file("jb2a.extras.tmfx.runes.circle.outpulse.tramnsmutation")
+            .atLocation(casterToken)
+            .duration(4000)
+            .fadeIn(500)
+            .fadeOut(500)
+            .scale(0.5)
+            .waitUntilFinished(-1000)
+            .filter("Glow", { color: 0xadd8e6 })
+        .effect()
+            .file("jb2a.impact.004.dark_purple")
+            .atLocation(targetId)
+            .fadeIn(100)
+            .fadeOut(200)
+        .effect()
+            .file("jb2a.markers.light.loop.blue")
+            .atLocation(targetId)
+            .fadeIn(100)
+            .fadeOut(200)
+            .belowTokens()
+            .scaleToObject(1.5)
+            .animateProperty("spriteContainer", "rotation", { from: 0, to: 360, duration: 5000, delay: 0 })
+    .play();
