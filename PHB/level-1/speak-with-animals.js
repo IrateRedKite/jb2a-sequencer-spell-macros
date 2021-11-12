437e8f953fb57d040b5de48a987ebd0bdfe3aec8
Rupert Walker <RupertCW@gmail.com>
2021-11-12 17:03:06 +0300

    Create speak-with-animals.js

---
+++ PHB/level-1/speak-with-animals.js
@@ -0,0 +1,35 @@
+const casterToken = canvas.tokens.get(args[0].tokenId);
+if (!casterToken) {
+	ui.notifications.warn("Please select a valid token to use this ability.");
+	return;
+}
+
+
+    new Sequence()
+        .effect()
+            .file("jb2a.extras.tmfx.runes.circle.outpulse.divination")
+            .atLocation(casterToken)
+            .duration(4000)
+            .fadeIn(500)
+            .fadeOut(500)
+            .scale(0.5)
+            .waitUntilFinished(-2000)
+            .filter("Glow", { color: 0x014421 })
+        .effect()
+            .file("jb2a.moonbeam.01.intro.green")
+            .atLocation(casterToken)
+            .fadeIn(100)
+            .fadeOut(200)
+            .belowTokens()
+            .scaleToObject(1.5)
+            .animateProperty("spriteContainer", "rotation", { from: 0, to: 360, duration: 5000, delay: 0 })
+        .effect()
+            .file("jb2a.shatter.green")
+            .delay(200)
+            .fadeIn(100)
+            .fadeOut(500)
+            .duration(1200)
+            .scale(0.4)
+            .opacity(0.4)
+            .atLocation(casterToken)
+    .play();
