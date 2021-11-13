8c2828dc933e109d8218f8ac00173291b2b04d24
Rupert Walker <RupertCW@gmail.com>
2021-11-13 12:10:56 +0300

    Swapped simple spells over to args-based casterToken const
    Changed `casterToken = canvas.tokens.controlled[0];` for `const casterToken = canvas.tokens.get(args[0].tokenId);` where appropriate for simple spells where little-no testing would be required. Some scripts need a bit more modification to switch over so there's still a few one the old system.

--- PHB/level-1/shield-start.js
+++ PHB/level-1/shield-start.js
@@ -1,4 +1,4 @@
-const casterToken = canvas.tokens.controlled[0];
+const casterToken = canvas.tokens.get(args[0].tokenId);
 if (!casterToken) {
 	ui.notifications.warn("Please select a valid token to use this ability.");
 	return;
