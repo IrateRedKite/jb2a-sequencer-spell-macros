8fa3f7fb731f80ff65f4d831a6015c72b8cad6a9
Rupert Walker <RupertCW@gmail.com>
2021-11-13 12:53:28 +0300

    Create dispel-magic.js

---
+++ PHB/level-3/dispel-magic.js
@@ -0,0 +1,31 @@
+const casterToken = canvas.tokens.get(args[0].tokenId);
+if (!casterToken) {
+	ui.notifications.warn("Please select a valid token to use this ability.");
+	return;
+}
+
+new Sequence()
+    .effect()
+        .file("jb2a.extras.tmfx.runes.circle.inpulse.abjuration")
+        .atLocation(casterToken)
+        .duration(1700)
+        .fadeIn(500)
+        .fadeOut(500)
+        .scale(0.5)
+        .filter("Glow", { color: 0xffffff })
+        .scaleIn(0, 500, {ease: "easeOutCubic", delay: 100})
+    .effect()
+        .file("jb2a.moonbeam.01.intro.rainbow")
+        .atLocation(casterToken)
+        .fadeIn(100)
+        .fadeOut(200)
+        .duration(1200)
+        .waitUntilFinished(-500)
+    .effect()
+        .file("jb2a.detect_magic.circle.grey")
+        .atLocation(casterToken)
+        .fadeIn(500)
+        .fadeOut(500)
+        .duration(2500)
+        .randomRotation() 
+    .play();
