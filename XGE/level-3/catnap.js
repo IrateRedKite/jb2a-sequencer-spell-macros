356507c160a416090accdc84fbb8d2413a23dd3c
Rupert Walker <RupertCW@gmail.com>
2021-11-13 13:01:59 +0300

    Create catnap.js

---
+++ XGE/level-3/catnap.js
@@ -0,0 +1,42 @@
+const casterToken = canvas.tokens.get(args[0].tokenId);
+if (!casterToken) {
+	ui.notifications.warn("Please select a valid token to use this ability.");
+	return;
+}
+
+new Sequence()
+    .effect()
+        .file("jb2a.extras.tmfx.runes.circle.inpulse.enchantment")
+        .atLocation(casterToken)
+        .duration(1700)
+        .fadeIn(500)
+        .fadeOut(500)
+        .scale(0.5)
+        .filter("Glow", { color: 0x98fb98 })
+        .scaleIn(0, 500, {ease: "easeOutCubic", delay: 100})
+    .effect()
+        .file("jb2a.moonbeam.01.intro.green")
+        .atLocation(casterToken)
+        .fadeIn(100)
+        .fadeOut(200)
+        .duration(1200)
+        .waitUntilFinished(-500)
+    .effect()
+        .file("jb2a.hunters_mark.pulse.02.green")
+        .atLocation(casterToken)
+        .duration(4000)
+        .fadeIn(500)
+        .fadeOut(500)
+        .scale(0.5)
+        .waitUntilFinished(-2000)
+        .filter("Glow", { color: 0xfefebe })
+        .opacity(0.5)
+    .effect()
+        .file("jb2a.detect_magic.circle.green")
+        .atLocation(casterToken)
+        .fadeIn(500)
+        .fadeOut(500)
+        .duration(2500)
+        .scale(0.5)
+        .randomRotation() 
+    .play();
