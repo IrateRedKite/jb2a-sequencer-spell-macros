21c896744980481a2d8faa586a604f97be140557
Rupert Walker <RupertCW@gmail.com>
2021-11-13 11:45:37 +0300

    Create goodberry.js

---
+++ PHB/level-1/goodberry.js
@@ -0,0 +1,30 @@
+const casterToken = canvas.tokens.get(args[0].tokenId);
+if (!casterToken) {
+	ui.notifications.warn("Please select a valid token to use this ability.");
+	return;
+}
+
+new Sequence()
+    .effect()
+        .file("jb2a.extras.tmfx.runes.circle.outpulse.tramnsmutation")
+        .atLocation(casterToken)
+        .duration(1700)
+        .fadeIn(500)
+        .fadeOut(500)
+        .scale(0.4)
+        .filter("Glow", { color: 0x98fb98 })
+        .scaleIn(0, 500, {ease: "easeOutCubic", delay: 100})
+    .effect()
+        .file("jb2a.moonbeam.01.intro.green")
+        .atLocation(casterToken)
+        .fadeIn(100)
+        .fadeOut(200)
+        .duration(1200)
+    .effect()
+        .file("jb2a.fireflies.many.01.green")
+        .atLocation(casterToken)
+        .fadeIn(700)
+        .fadeOut(700)
+        .duration(4000)
+        .scale(0.4)
+    .play();
