bc929f1c79874262db5b76f76e12fdf99b44a585
Paul Bierton <paul.bierton@vulcan-eng.com>
2021-11-23 08:57:21 +0000

    Fireball Commends

--- PHB/cantrips/eldritch-blast.js
+++ PHB/cantrips/eldritch-blast.js
@@ -1,3 +1,5 @@
+//Requires MIDI-QoL if you want the animation to play only on hit. 
+
 const casterToken = canvas.tokens.get(args[0].tokenId);
 
 if (!casterToken) {
@@ -8,24 +10,24 @@ let target = Array.from(game.user.targets)[0];
 
 new Sequence()
     .effect()
-        .file("jb2a.extras.tmfx.runes.circle.outpulse.evocation")
-        .atLocation(casterToken)
-        .duration(2000)
-        .fadeIn(500)
-        .fadeOut(500)
-        .scale(0.5)
-        .filter("Glow", { color: 0xff0000 })
-        .waitUntilFinished(-500)
+    .file("jb2a.extras.tmfx.runes.circle.outpulse.evocation")
+    .atLocation(casterToken)
+    .duration(2000)
+    .fadeIn(500)
+    .fadeOut(500)
+    .scale(0.5)
+    .filter("Glow", { color: 0xff0000 })
+    .waitUntilFinished(-500)
 
+.effect()
+    .file("jb2a.energy_strands.complete.dark_red.01")
+    .atLocation(casterToken)
+    .fadeIn(500)
+    .fadeOut(500)
+    .scale(0.5)
     .effect()
-        .file("jb2a.energy_strands.complete.dark_red.01")
-        .atLocation(casterToken)
-        .fadeIn(500)
-        .fadeOut(500)
-        .scale(0.5)
-        .effect()
-        .file("jb2a.eldritch_blast.dark_red")
-        .atLocation(casterToken)
-        .reachTowards(target)
-        .playIf(args[0].hitTargets.length === 1) // Comment this line out if not using MIDI
+    .file("jb2a.eldritch_blast.dark_red")
+    .atLocation(casterToken)
+    .reachTowards(target)
+    .playIf(args[0].hitTargets.length === 1) // Comment this line out if not using MIDI
     .play();
