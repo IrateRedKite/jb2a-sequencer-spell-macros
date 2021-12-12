cb48e78a7ef4059daf5f6db09e5a77559ce667ba
Rupert Walker <RupertCW@gmail.com>
2021-12-12 15:12:57 +0300

    Broke bless.js up into two macros so it functions even if a player doesn't have their token selected.

--- PHB/level-1/bless.js
+++ PHB/level-1/bless.js
@@ -1,27 +1,11 @@
 //NOTE: This needs to go into the effect's macro.execute property, rather than midi's 'On Item Use' field.
-
-const casterToken = canvas.tokens.controlled[0];
-if (!casterToken) {
-	ui.notifications.warn("Please select a valid token to use this ability.");
-	return;
-}
 let targetID = canvas.tokens.get(args[1].tokenId);
 
 if(args[0] === "on"){
     // If the dynamic active effect started
 
     new Sequence()
-        .effect()
-            .file("jb2a.extras.tmfx.runes.circle.outpulse.enchantment")
-            .atLocation(casterToken)
-            .duration(4000)
-            .fadeIn(500)
-            .fadeOut(500)
-            .scale(0.5)
-            .waitUntilFinished(-2000)
-            .filter("Glow", { color: 0xfefebe })
-            .opacity(0.5)
-        .effect()
+         .effect()
             .file("jb2a.extras.tmfx.outpulse.circle.01.slow")
             .atLocation(targetID)
             .fadeIn(500)
