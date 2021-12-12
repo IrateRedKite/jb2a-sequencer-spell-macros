8b8542a55102828fbbe27102c90981aa14b5da5e
Rupert Walker <RupertCW@gmail.com>
2021-12-12 15:22:17 +0300

    Broke bane.js up into two two macros so it functions even if a player doesn't have their token selected.

--- PHB/level-1/bane.js
+++ PHB/level-1/bane.js
@@ -1,30 +1,18 @@
 //NOTE: This needs to go into the effect's macro.execute property, rather than midi's 'On Item Use' field.
 
-const casterToken = canvas.tokens.controlled[0];
-if (!casterToken) {
-	ui.notifications.warn("Please select a valid token to use this ability.");
-	return;
-}
 let targetID = canvas.tokens.get(args[1].tokenId);
 
 if(args[0] === "on"){
     // If the dynamic active effect started
 
-    new Sequence()
-        .effect()
-            .file("jb2a.extras.tmfx.runes.circle.outpulse.enchantment")
-            .atLocation(casterToken)
-            .duration(4000)
-            .fadeIn(500)
-            .fadeOut(500)
-            .scale(0.5)
-            .waitUntilFinished(-2000)
-            .filter("Glow", { color: 0xf02d2b })    
+    new Sequence() 
         .effect()
             .file("jb2a.impact.004.dark_red")
             .atLocation(targetID)
             .fadeIn(500)
+            .delay(2000)
         .effect()
+            .delay(2000)
             .file("jb2a.energy_strands.overlay.dark_red.01")
             .fadeIn(100)
             .fadeOut(500)
