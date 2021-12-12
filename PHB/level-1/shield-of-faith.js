2df468f624dbce0f8cdd15f171ea85cb8ba27ab5
Rupert Walker <RupertCW@gmail.com>
2021-12-12 17:39:57 +0300

    Split shield-of-faith.js into two macros

--- PHB/level-1/shield-of-faith.js
+++ PHB/level-1/shield-of-faith.js
@@ -1,33 +1,10 @@
 //NOTE: This needs to go into the effect's macro.execute property, rather than midi's 'On Item Use' field.
 
-const casterToken = canvas.tokens.controlled[0];
-if (!casterToken) {
-	ui.notifications.warn("Please select a valid token to use this ability.");
-	return;
-}
-
 let tokenD = canvas.tokens.get(args[1].tokenId);
 
 if(args[0] === "on"){
      // If the dynamic active effect started
     new Sequence()
-        .effect()
-            .file("jb2a.extras.tmfx.runes.circle.inpulse.abjuration")
-            .atLocation(casterToken)
-            .duration(2000)
-            .fadeIn(500)
-            .fadeOut(500)
-            .scale(0.5)
-            .opacity(0.3)
-            .filter("Glow", { color: 0xffffff })
-            .scaleIn(0, 500, {ease: "easeOutCubic", delay: 100})
-        .effect()
-            .file("jb2a.moonbeam.01.intro.rainbow")
-            .atLocation(casterToken)
-            .fadeIn(100)
-            .fadeOut(200)
-            .duration(1200)
-            .waitUntilFinished(-500)
         .effect()
             .file("jb2a.shield.02.intro.yellow")
             .attachTo(tokenD)
