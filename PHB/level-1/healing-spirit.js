56e12843b9d1520ffdfab26266fd5011dade56d0
Rupert Walker <RupertCW@gmail.com>
2021-12-17 23:08:41 +0300

    Create healing-spirit.js

---
+++ PHB/level-1/healing-spirit.js
@@ -0,0 +1,42 @@
+const casterToken = canvas.tokens.get(args[0].tokenId);
+if (!casterToken) {
+	ui.notifications.warn("Please select a valid token to use this ability.");
+	return;
+}
+
+await warpgate.spawn("Healing Spirit", {}, {
+    pre: async (location) => {
+        const seq = new Sequence()
+        .effect()
+        .file("jb2a.extras.tmfx.runes.circle.outpulse.conjuration")
+        .atLocation(casterToken)
+        .duration(2000)
+        .fadeIn(500)
+        .fadeOut(500)
+        .scale(0.5)
+        .opacity(0.3)
+        .filter("Glow", { color: 0x7D87BF })
+        .scaleIn(0, 500, {ease: "easeOutCubic", delay: 100})
+    .effect()
+        .file("jb2a.moonbeam.01.intro.blue")
+        .atLocation(casterToken)
+        .fadeIn(100)
+        .fadeOut(200)
+        .duration(1200)
+        .waitUntilFinished(-500)
+    .effect()
+        .file("jb2a.toll_the_dead.blue.shockwave")
+        .atLocation(location)
+        .fadeIn(500)
+        .fadeOut(500)
+        .scale(0.5)
+        .scaleIn(0, 500, {ease: "easeOutCubic", delay: 100})
+
+    seq.play();
+
+        // Sleep for 500ms
+        await (new Promise(resolve => setTimeout(resolve, 1200)));
+    }
+}, {
+    collision: false
+});
