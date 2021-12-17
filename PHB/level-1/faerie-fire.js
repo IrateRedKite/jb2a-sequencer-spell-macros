f18b6928057ec00daf486828c465944f7c952d70
Rupert Walker <RupertCW@gmail.com>
2021-12-17 23:08:47 +0300

    Create faerie-fire.js

---
+++ PHB/level-1/faerie-fire.js
@@ -0,0 +1,31 @@
+//NOTE: This needs to go into the effect's macro.execute property, rather than midi's 'On Item Use' field.
+
+let targetID = canvas.tokens.get(args[1].tokenId);
+
+if(args[0] === "on"){
+    // If the dynamic active effect started
+
+    new Sequence() 
+        .effect()
+            .file("jb2a.impact.007.blue")
+            .atLocation(targetID)
+            .fadeIn(500)
+            .delay(2000)
+        .effect()
+            .delay(2000)
+            .file("jb2a.markers.light.loop.blue")
+            .fadeIn(100)
+            .fadeOut(500)
+            .scale(0.4, 0.45)
+            .attachTo(targetID)
+            .randomRotation()
+            .scaleIn(0, 500, {ease: "easeOutCubic"})
+            .persist()
+            .name(`faerie-fire-${targetID.id}`)
+    .play();
+
+}
+    if(args[0] === "off"){
+        // If the dynamic active effect ended
+        Sequencer.EffectManager.endEffects({ name: `faerie-fire-${targetID.id}`, object: targetID });
+    }    
