88b2d64fdb576d8b544802c049d2b6db48a6053c
Rupert Walker <RupertCW@gmail.com>
2021-12-17 23:08:49 +0300

    Create faerie-fire-start.js

---
+++ PHB/level-1/faerie-fire-start.js
@@ -0,0 +1,16 @@
+const casterToken = canvas.tokens.get(args[0].tokenId);
+if (!casterToken) {
+	ui.notifications.warn("Please select a valid token to use this ability.");
+	return;
+}
+    new Sequence()
+        .effect()
+            .file("jb2a.extras.tmfx.runes.circle.outpulse.evocation")
+            .atLocation(casterToken)
+            .duration(4000)
+            .fadeIn(500)
+            .fadeOut(500)
+            .scale(0.5)
+            .waitUntilFinished(-2000)
+            .filter("Glow", { color: 0x7D87BF }) 
+    .play();
