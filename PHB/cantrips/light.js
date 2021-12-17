34002a81d531fe399312a3a4decea24f1c9027d2
Rupert Walker <RupertCW@gmail.com>
2021-12-17 14:23:19 +0300

    Create light.js

---
+++ PHB/cantrips/light.js
@@ -0,0 +1,28 @@
+const casterToken = canvas.tokens.get(args[0].tokenId);
+if (!casterToken) {
+	ui.notifications.warn("Please select a valid token to use this ability.");
+	return;
+}
+
+new Sequence()
+    .effect()
+        .file("jb2a.extras.tmfx.runes.circle.outpulse.evocation")
+        .atLocation(casterToken)
+        .duration(1700)
+        .fadeIn(500)
+        .fadeOut(500)
+        .scale(0.5)
+        .filter("Glow", { color: 0xffa500 })
+        .scaleIn(0, 500, {ease: "easeOutCubic", delay: 100})
+    .effect()
+        .file("jb2a.moonbeam.01.intro.rainbow")
+        .atLocation(casterToken)
+        .fadeIn(100)
+        .fadeOut(200)
+        .duration(1200)
+        .effect()
+    .file("jb2a.impact.yellow.5")
+        .atLocation(casterToken)
+        .fadeIn(100)
+        .fadeOut(200)
+    .play();
