e3290144addac69cd7ce7c8c0f981a397f564598
Rupert Walker <RupertCW@gmail.com>
2022-03-22 10:03:05 +0300

    Added some placeholders for detect spells until ASE can clone detect-magic.js

---
+++ Spells/PHB/level-1/detect-evil.js
@@ -0,0 +1,31 @@
+const casterToken = canvas.tokens.get(args[0].tokenId);
+if (!casterToken) {
+	ui.notifications.warn("Please select a valid token to use this ability.");
+	return;
+}
+
+new Sequence()
+    .effect()
+        .file("jb2a.extras.tmfx.runes.circle.inpulse.divination")
+        .atLocation(casterToken)
+        .duration(1700)
+        .fadeIn(500)
+        .fadeOut(500)
+        .scale(0.5)
+        .filter("Glow", { color: 0x8b0000 })
+        .scaleIn(0, 500, {ease: "easeOutCubic", delay: 100})
+    .effect()
+        .file("jb2a.moonbeam.01.intro.blue")
+        .atLocation(casterToken)
+        .fadeIn(100)
+        .fadeOut(200)
+        .duration(1200)
+        .waitUntilFinished(-500)
+    .effect()
+        .file("jb2a.detect_magic.circle.purple")
+        .atLocation(casterToken)
+        .fadeIn(500)
+        .fadeOut(500)
+        .duration(2500)
+        .randomRotation() 
+    .play();
